6546c4acbc836dc5b7bde4c7580584ad3938881e
Thowhid31 <thowhidantor16@gmail.com>
2022-05-27 23:35:37 +0600

    added handle user to user

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
 
+
+interface User{
+    name?: string,
+    job?: string
+}
+
 const Counter = () => {
 
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState<number>(0);
+    const [user, setUser] = useState<User | null>(null)
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const userData = {
+            name: 'Admin',
+            job: 'Developer'
+        }
+        setUser(userData)
+    }
 
 
     const handleIncrease = (): void => {
         setCount(count + 1)
+    }
+    const handleDecrease = (): void => {
+        setCount(count - 1)
     }
 
     return (
@@ -14,6 +33,7 @@ const Counter = () => {
             <h2>Hello TypeScript!</h2>
             <h1>{count}</h1>
             <button onClick={handleIncrease}>Increase</button>
+            <button onClick={handleDecrease}>Decrease</button>
         </div>
     );
 };
