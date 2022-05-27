1fdba6efb0e3c26f2a9dff1dcd5c7213b71a077c
Thowhid31 <thowhidantor16@gmail.com>
2022-05-28 00:04:12 +0600

    adde user button with TS

--- src/components/Use.tsx
+++ src/components/Use.tsx
@@ -2,15 +2,17 @@ import React, { FC } from 'react';
 
 interface Props{
     name: string,
-    age: number
+    age: number,
+    addUser: () => void
 }
 
-const Use: FC<Props> = ({name, age}) => {
+const Use: FC<Props> = ({name, age, addUser}) => {
 
     
     return (
         <div>
             <h2>Hello from: {name} with Experience {age}</h2>
+            <button onClick={addUser}>Add me</button>
         </div>
     );
 };
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import Use from './Use';
 
 const Users = () => {
+
+    const handleAddUser = ():void => {
+
+    }
+
     return (
         <div>
             <h1>My Users</h1>
-            <Use name='Rohomot' age={55}></Use>
+            <Use name='Rohomot' age={55} addUser={handleAddUser}></Use>
         </div>
     );
 };
